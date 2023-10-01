123009413cc3dd263517387422a47a28d2e619ce
Icheng Lin <fireclaws9@gmail.com>
2023-10-01 14:50:19 -0500

    Revamped System Log

--- utilities/system_log.ts
+++ utilities/system_log.ts
@@ -3,7 +3,7 @@ import chalk from "chalk";
 export default class SystemLog {
 
     public static log_send(log_message: string, log_type: string = "LOG") {
-        const log_signature = chalk.blue(`[${chalk.green.bold(log_type)} ${new Date().toLocaleTimeString()}]`);
+        const log_signature = chalk.blue(`[${(log_type === "LOG") ? chalk.green.bold(log_type) : chalk.red.bold(log_type)} ${new Date().toLocaleTimeString()}]`);
         console.log(chalk.white(`${log_signature} ${log_message.replace(/\n/g, `\n${log_signature} `)}`));
     }
 
