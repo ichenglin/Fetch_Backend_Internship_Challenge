31c44e8c2996ed8fa995e5775ccaf26a248fe402
Icheng Lin <fireclaws9@gmail.com>
2023-10-01 12:51:13 -0500

    Added Chalk

--- utilities/system_log.ts
+++ utilities/system_log.ts
@@ -1,8 +1,10 @@
+import chalk from "chalk";
+
 export default class SystemLog {
 
     public static log_send(log_message: string, log_type: string = "LOG") {
-        const log_signature = `[${log_type} ${new Date().toLocaleTimeString()}]`;
-        console.log(`${log_signature} ${log_message.replace(/\n/g, `\n${log_signature} `)}`);
+        const log_signature = chalk.blue(`[${chalk.green.bold(log_type)} ${new Date().toLocaleTimeString()}]`);
+        console.log(chalk.white(`${log_signature} ${log_message.replace(/\n/g, `\n${log_signature} `)}`));
     }
 
 }
